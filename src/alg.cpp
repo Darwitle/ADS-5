b62a397fc47f04210eee9987e884a23edd8e91ea
Darwitle <margonesnanowa@gmail.com>
2025-06-07 21:33:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,109 +4,109 @@
 #include "tstack.h"
 
 int priority(char sym) {
-switch (sym) {
-case '(': return 0;
-case ')': return 1;
-case '+': return 2;
-case '-': return 2;
-case '*': return 3;
-case '/': return 3;
-default : return -1;
-}
+  switch (sym) {
+      case '(': return 0;
+      case ')': return 1;
+      case '+': return 2;
+      case '-': return 2;
+      case '*': return 3;
+      case '/': return 3;
+      default : return -1;
+  }
 }
 
 std::string infx2pstfx(const std::string& inf) {
-std::string postfix = ""; // NOLINT(runtime/string)
-TStack<char, 100> transStack;
+  std::string spostfix = "";
+  TStack<char, 100> transStack;
 
-for (int i = 0; i < inf.length(); ++i) {
-char ccur = inf[i];
-if (isdigit(ccur)) {
-    std::string num;
-    while (i < inf.length() && isdigit(inf[i])) {
-        num += inf[i];
-        i++;
-    }
-    i--;
-    postfix += num + " ";
-} else if (ccur == '(') {
-    transStack.push(ccur);
-} else if (ccur == ')') {
-    while (!transStack.isEmpty() && transStack.get() != '(') {
-        postfix += transStack.pop();
-        postfix += " ";
-    }
-    if (!transStack.isEmpty() && transStack.get() == '(') {
-        transStack.pop();
-    }
-} else if (ccur == '+' || ccur == '-' || ccur == '*' || ccur == '/') {
-    while (!transStack.isEmpty()
-      && priority(ccur) <= priority(transStack.get())) {
-        postfix += transStack.pop();
-        postfix += " ";
-    }
-    transStack.push(ccur);
-} else if (ccur == ' ') {
-    continue;
-}
-}
-while (!transStack.isEmpty()) {
-postfix += transStack.pop();
-postfix += " ";
-}
-if (!postfix.empty() && postfix.back() == ' ') {
-postfix.pop_back();
-}
-return postfix;
+  for (int i = 0; i < inf.length(); ++i) {
+      char cur = inf[i];
+      if (isdigit(cur)) {
+          std::string num;
+          while (i < inf.length() && isdigit(inf[i])) {
+              num += inf[i];
+              i++;
+          }
+          i--;
+          spostfix += num + " ";
+      } else if (cur == '(') {
+          transStack.push(cur);
+      } else if (cur == ')') {
+          while (!transStack.isEmpty() && transStack.get() != '(') {
+              spostfix += transStack.pop();
+              spostfix += " ";
+          }
+          if (!transStack.isEmpty() && transStack.get() == '(') {
+              transStack.pop();
+          }
+      } else if (cur == '+' || cur == '-' || cur == '*' || cur == '/') {
+          while (!transStack.isEmpty()
+            && priority(cur) <= priority(transStack.get())) {
+              spostfix += transStack.pop();
+              spostfix += " ";
+          }
+          transStack.push(cur);
+      } else if (cur == ' ') {
+          continue;
+      }
+  }
+  while (!transStack.isEmpty()) {
+      spostfix += transStack.pop();
+      spostfix += " ";
+  }
+  if (!postfix.empty() && spostfix.back() == ' ') {
+     spostfix.pop_back();
+  }
+    return spostfix;
 }
 
 int eval(const std::string& post) {
-TStack<int, 100> countStack;
-std::string currentNumber; // NOLINT(runtime/string)
-for (int i = 0; i < post.length(); ++i) {
-char ccurSym = post[i];
-if (isdigit(ccurSym)) {
-    currentNumber += ccurSym;
-} else if (ccurSym == ' ') {
-    if (!currentNumber.empty()) {
-        countStack.push(stoi(currentNumber));
-        currentNumber.clear();
-    }
-} else if (ccurSym == '+' || ccurSym == '-'
-  || ccurSym == '*' || ccurSym == '/') {
-    if (countStack.isEmpty()) {
-        return 0;
-    }
-    int operand2 = countStack.pop();
-    if (countStack.isEmpty()) {
-        return 0;
-    }
-    int operand1 = countStack.pop();
-    int result;
-    switch (ccurSym) {
-        case '+':
-            result = operand1 + operand2;
-            break;
-        case '-':
-            result = operand1 - operand2;
-            break;
-        case '*':
-            result = operand1 * operand2;
-            break;
-        case '/':
-            if (operand2 == 0) {
-                 return 0;
-            }
-            result = operand1 / operand2;
-            break;
-        default:
-            return 0;
-    }
-    countStack.push(result);
-}
-}
-if (countStack.isEmpty()) {
-return 0;
-}
-return countStack.pop();
+  TStack<int, 100> countStack;
+  std::string currentNumber;
+  for (int i = 0; i < post.length(); ++i) {
+      char curSym = post[i];
+      if (isdigit(curSym)) {
+          currentNumber += curSym;
+      } else if (curSym == ' ') {
+          if (!currentNumber.empty()) {
+              countStack.push(stoi(currentNumber));
+              currentNumber.clear();
+          }
+      } else if (curSym == '+' || curSym == '-'
+        || curSym == '*' || curSym == '/') {
+          if (countStack.isEmpty()) {
+              return 0;
+          }
+          int operand2 = countStack.pop();
+          if (countStack.isEmpty()) {
+              return 0;
+          }
+          int operand1 = countStack.pop();
+          int result;
+          switch (curSym) {
+              case '+':
+                  result = operand1 + operand2;
+                  break;
+              case '-':
+                  result = operand1 - operand2;
+                  break;
+              case '*':
+                  result = operand1 * operand2;
+                  break;
+              case '/':
+                  if (operand2 == 0) {
+                       return 0;
+                  }
+                  result = operand1 / operand2;
+                  break;
+              default:
+                  return 0;
+          }
+          countStack.push(result);
+      }
+  }
+  if (countStack.isEmpty()) {
+      return 0;
+  }
+  return countStack.pop();
 }
